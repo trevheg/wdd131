93c8ebbb8f708c087604f979d340853d4df0be7a
trevheg <trevorhegstrom@gmail.com>
2025-03-12 22:17:59 -0700

    js added

---
+++ scripts/getdates.js
@@ -0,0 +1,14 @@
+// get elements from the DOM
+const currentYear = document.querySelector("#currentYear");
+const lastModified = document.querySelector("#lastModified");
+
+// get a date object
+const today = new Date();
+
+// put the current year in the web page
+currentYear.innerHTML = today.getFullYear();
+
+// put the last modified date in the page
+lastModified.innerHTML = (Date(document.lastModified))
+
+
