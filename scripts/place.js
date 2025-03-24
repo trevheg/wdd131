a5d24136cd4d91a434659c82eef67e0c67f3c81a
trevheg <trevorhegstrom@gmail.com>
2025-03-24 15:11:41 -0700

    upload place for audit

--- scripts/place.js
+++ scripts/place.js
@@ -3,14 +3,22 @@ const pageConditions = document.querySelector("#conditions");
 const pageWindSpeed = document.querySelector("#wind-speed");
 const pageWindChill = document.querySelector("#wind-chill");
 
-let temperature = 10
-let conditions = "Kaijuu Attack"
-let windSpeed = 10
+let temperature = 10;
+let conditions = "Kaijuu Attack";
+let windSpeed = 10;
 
 function calculateWindChill(temperature, windSpeed) {
-    return 0;
+    return (13.12 + (0.6215 * temperature) - (11.37 * (windSpeed**0.16)) + ((0.3965 * temperature) * (windSpeed**0.16))).toFixed(1);
 }
+
 
 pageTemperature.textContent = `${temperature}°C`;
 pageConditions.textContent = `${conditions}`;
 pageWindSpeed.textContent = `${windSpeed} km/h`;
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    pageWindChill.textContent = `${calculateWindChill(temperature, windSpeed)}°C`;
+} else {
+    pageWindChill.textContent = `N/A`;
+};
+
