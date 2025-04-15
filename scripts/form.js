3b6eb97cdc695b71f0be9df4b89858f7d244d266
trevheg <trevorhegstrom@gmail.com>
2025-04-15 14:13:42 -0700

    js for testing

---
+++ scripts/form.js
@@ -0,0 +1,40 @@
+const select = document.querySelector("product");
+
+
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+products.forEach(product => {
+  // const dropDown = document.querySelector("product");
+    let option = document.createElement("option");
+    option.text = product.name;
+    option.value = product.id;
+    select.appendChild(option);
+})
+
